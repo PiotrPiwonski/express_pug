2ebc4da5b506d67617e972e11dcd09d272089dbf
Lenovo <piwonskipiotrp>
2022-01-13 17:11:10 +0100

    admin login, admin news

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 // Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 var createError = require('http-errors');
+var cookieSession = require('cookie-session');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var config = require('./config');
+var mongoose = require('mongoose');
+
+
+mongoose.connect(config.db, {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+// db.once('open', function () {
+//   console.log('connected');
+// });
 
 var indexRouter = require('./routes/index');
 var newsRouter = require('./routes/news');
 var quizRouter = require('./routes/quiz');
 var adminRouter = require('./routes/admin');
+const {log} = require("debug");
+
 
 
 var app = express();
@@ -24,6 +38,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name: 'session',
+  keys: config.keySession,
+  maxAge: config.maxAgeSession,
+}))
 
 app.use(function(req, res, next) {
   res.locals.path = req.path;
@@ -35,6 +54,7 @@ app.use('/', indexRouter);
 app.use('/news', newsRouter);
 app.use('/quiz', quizRouter);
 app.use('/admin', adminRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    db: 'mongodb+srv://admin:uJT0djd1IpHIhYTM@cluster0.h7zq1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000,
+}
+
+// mongodb+srv://admin:<password>@cluster0.h7zq1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// uJT0djd1IpHIhYTM
